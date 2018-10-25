2eb4052a301c9fcdb11ceb7705b4311ff64b0f39
Vishal Roy <vishal.kr.roy@gmail.com>
2018-10-25 16:00:15 +0530

    Done Boy

--- OddEven.py
+++ OddEven.py
@@ -4,5 +4,5 @@ num = input("Enter a number: ")
 mod = num % 2
 if mod > 0:
     print("You picked an odd number.")
-else
-    print("You picked an even number.
+else:
+    print("You picked an even number.")
